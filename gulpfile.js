12f0d7d08858ff4c1244b02d1c0864f29faa4649
James Ferguson <jamesfergusonx@gmail.com>
2015-05-04 06:55:34 -0700

    Coveralls update

--- gulpfile.js
+++ gulpfile.js
@@ -27,6 +27,9 @@ var pngquant = require('imagemin-pngquant');
 var terminus = require('terminus');
 var runSequence = require('run-sequence');
 
+var istanbul = require('gulp-istanbul');
+var mocha = require('gulp-mocha');
+
 /**
  * Banner
  */
@@ -392,6 +395,18 @@ gulp.task('desktop', ['mobile'], function (cb) {
 });
 
 gulp.task('pagespeed', ['desktop']);
+
+gulp.task('test', function (cb) {
+  gulp.src(['lib/**/*.js', 'main.js'])
+    .pipe(istanbul()) // Covering files
+    .pipe(istanbul.hookRequire()) // Force `require` to return covered files
+    .on('finish', function () {
+      gulp.src(['test/*.js'])
+        .pipe(mocha())
+        .pipe(istanbul.writeReports()) // Creating the reports after tests runned
+        .on('end', cb);
+    });
+});
 
 // var statics = {
 //   my: 'statics',
